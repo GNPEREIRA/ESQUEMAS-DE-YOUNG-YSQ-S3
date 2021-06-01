0138a0061f19487944c800e2d33f4ce2126ce48a
GNPEREIRA <guspereira2010@gmail.com>
2021-06-01 09:12:08 -0300

    Questionário de esquemas de young ysq-s3

---
+++ script.js
@@ -0,0 +1,379 @@
+function calcular(){
+        
+    let resultado = document.querySelector('#resultado');
+    var total=0;
+    var radio1 = document.querySelector('input[name = resposta1]:checked').value;
+    var radio2 = document.querySelector('input[name = resposta2]:checked').value;
+    var radio3 = document.querySelector('input[name = resposta3]:checked').value;
+    var radio4 = document.querySelector('input[name = resposta4]:checked').value;
+    var radio5 = document.querySelector('input[name = resposta5]:checked').value;
+    var radio6 = document.querySelector('input[name = resposta6]:checked').value;
+    var radio7 = document.querySelector('input[name = resposta7]:checked').value;
+    var radio8 = document.querySelector('input[name = resposta8]:checked').value;
+    var radio9 = document.querySelector('input[name = resposta9]:checked').value;
+    var radio10 = document.querySelector('input[name = resposta10]:checked').value;
+    var radio11 = document.querySelector('input[name = resposta11]:checked').value;
+    var radio12 = document.querySelector('input[name = resposta12]:checked').value;
+    var radio13 = document.querySelector('input[name = resposta13]:checked').value;
+    var radio14 = document.querySelector('input[name = resposta14]:checked').value;
+    var radio15 = document.querySelector('input[name = resposta15]:checked').value;
+    var radio16 = document.querySelector('input[name = resposta16]:checked').value;
+    var radio17 = document.querySelector('input[name = resposta17]:checked').value;
+    var radio18 = document.querySelector('input[name = resposta18]:checked').value;
+    var radio19 = document.querySelector('input[name = resposta19]:checked').value;
+    var radio20 = document.querySelector('input[name = resposta20]:checked').value;
+    var radio21 = document.querySelector('input[name = resposta21]:checked').value;
+    var radio22 = document.querySelector('input[name = resposta22]:checked').value;
+    var radio23 = document.querySelector('input[name = resposta23]:checked').value;
+    var radio24 = document.querySelector('input[name = resposta24]:checked').value;
+    var radio25 = document.querySelector('input[name = resposta25]:checked').value;
+    var radio26 = document.querySelector('input[name = resposta26]:checked').value;
+    var radio27 = document.querySelector('input[name = resposta27]:checked').value;
+    var radio28 = document.querySelector('input[name = resposta28]:checked').value;
+    var radio29 = document.querySelector('input[name = resposta29]:checked').value;
+    var radio30 = document.querySelector('input[name = resposta30]:checked').value;
+    var radio31 = document.querySelector('input[name = resposta31]:checked').value;
+    var radio32 = document.querySelector('input[name = resposta32]:checked').value;
+    var radio33 = document.querySelector('input[name = resposta33]:checked').value;
+    var radio34 = document.querySelector('input[name = resposta34]:checked').value;
+    var radio35 = document.querySelector('input[name = resposta35]:checked').value;
+    var radio36 = document.querySelector('input[name = resposta36]:checked').value;
+    var radio37 = document.querySelector('input[name = resposta37]:checked').value;
+    var radio38 = document.querySelector('input[name = resposta38]:checked').value;
+    var radio39 = document.querySelector('input[name = resposta39]:checked').value;
+    var radio40 = document.querySelector('input[name = resposta40]:checked').value;
+    var radio41 = document.querySelector('input[name = resposta41]:checked').value;
+    var radio42 = document.querySelector('input[name = resposta42]:checked').value;
+    var radio43 = document.querySelector('input[name = resposta43]:checked').value;
+    var radio44 = document.querySelector('input[name = resposta44]:checked').value;
+    var radio45 = document.querySelector('input[name = resposta45]:checked').value;
+    var radio46 = document.querySelector('input[name = resposta46]:checked').value;
+    var radio47 = document.querySelector('input[name = resposta47]:checked').value;
+    var radio48 = document.querySelector('input[name = resposta48]:checked').value;
+    var radio49 = document.querySelector('input[name = resposta49]:checked').value;
+    var radio50 = document.querySelector('input[name = resposta50]:checked').value;
+    var radio51 = document.querySelector('input[name = resposta51]:checked').value;
+    var radio52 = document.querySelector('input[name = resposta52]:checked').value;
+    var radio53 = document.querySelector('input[name = resposta53]:checked').value;
+    var radio54 = document.querySelector('input[name = resposta54]:checked').value;
+    var radio55 = document.querySelector('input[name = resposta55]:checked').value;
+    var radio56 = document.querySelector('input[name = resposta56]:checked').value;
+    var radio57 = document.querySelector('input[name = resposta57]:checked').value;
+    var radio58 = document.querySelector('input[name = resposta58]:checked').value;
+    var radio59 = document.querySelector('input[name = resposta59]:checked').value;
+    var radio60 = document.querySelector('input[name = resposta60]:checked').value;
+    var radio61 = document.querySelector('input[name = resposta61]:checked').value;
+    var radio62 = document.querySelector('input[name = resposta62]:checked').value;
+    var radio63 = document.querySelector('input[name = resposta63]:checked').value;
+    var radio64 = document.querySelector('input[name = resposta64]:checked').value;
+    var radio65 = document.querySelector('input[name = resposta65]:checked').value;
+    var radio66 = document.querySelector('input[name = resposta66]:checked').value;
+    var radio67 = document.querySelector('input[name = resposta67]:checked').value;
+    var radio68 = document.querySelector('input[name = resposta68]:checked').value;
+    var radio69 = document.querySelector('input[name = resposta69]:checked').value;
+    var radio70 = document.querySelector('input[name = resposta70]:checked').value;
+    var radio71 = document.querySelector('input[name = resposta71]:checked').value;
+    var radio72 = document.querySelector('input[name = resposta72]:checked').value;
+    var radio73 = document.querySelector('input[name = resposta73]:checked').value;
+    var radio74 = document.querySelector('input[name = resposta74]:checked').value;
+    var radio75 = document.querySelector('input[name = resposta75]:checked').value;
+    var radio76 = document.querySelector('input[name = resposta76]:checked').value;
+    var radio77 = document.querySelector('input[name = resposta77]:checked').value;
+    var radio78 = document.querySelector('input[name = resposta78]:checked').value;
+    var radio79 = document.querySelector('input[name = resposta79]:checked').value;
+    var radio80 = document.querySelector('input[name = resposta80]:checked').value;
+    var radio81 = document.querySelector('input[name = resposta81]:checked').value;
+    var radio82 = document.querySelector('input[name = resposta82]:checked').value;
+    var radio83 = document.querySelector('input[name = resposta83]:checked').value;
+    var radio84 = document.querySelector('input[name = resposta84]:checked').value;
+    var radio85 = document.querySelector('input[name = resposta85]:checked').value;
+    var radio86 = document.querySelector('input[name = resposta86]:checked').value;
+    var radio87 = document.querySelector('input[name = resposta87]:checked').value;
+    var radio88 = document.querySelector('input[name = resposta88]:checked').value;
+    var radio89 = document.querySelector('input[name = resposta89]:checked').value;
+    var radio90 = document.querySelector('input[name = resposta90]:checked').value;
+     
+    if (radio1){
+        radio1 = parseInt(radio1)
+    }
+    if (radio2){
+        radio2 = parseInt(radio2)
+    }
+    if (radio3){
+        radio3 = parseInt(radio3)
+    }
+    if (radio4){
+        radio4 = parseInt(radio4)
+    }
+    if (radio5){
+        radio5 = parseInt(radio5)
+    }
+    if (radio6){
+        radio6 = parseInt(radio6)
+    }
+    if (radio7){
+        radio7 = parseInt(radio7)
+    }
+    if (radio8){
+        radio8 = parseInt(radio8)
+    }
+    if (radio9){
+        radio9 = parseInt(radio9)
+    }
+    if (radio10){
+        radio10 = parseInt(radio10)
+    }
+    if (radio11){
+        radio11 = parseInt(radio11)
+    }
+    if (radio12){
+        radio12 = parseInt(radio12)
+    }
+    if (radio13){
+        radio13 = parseInt(radio13)
+    }
+    if (radio14){
+        radio14 = parseInt(radio14)
+    }
+    if (radio15){
+        radio15 = parseInt(radio15)
+    }
+    if (radio16){
+        radio16 = parseInt(radio16)
+    }
+    if (radio17){
+        radio17 = parseInt(radio17)
+    }
+    if (radio18){
+        radio18 = parseInt(radio18)
+    }
+    if (radio19){
+        radio19 = parseInt(radio19)
+    }
+    if (radio20){
+        radio20 = parseInt(radio20)
+    }
+    if (radio21){
+        radio21 = parseInt(radio21)
+    }
+    if (radio22){
+        radio22 = parseInt(radio22)
+    }
+    if (radio23){
+        radio23 = parseInt(radio23)
+    }
+    if (radio24){
+        radio24 = parseInt(radio24)
+    }
+    if (radio25){
+        radio25 = parseInt(radio25)
+    }
+    if (radio26){
+        radio26 = parseInt(radio26)
+    }
+    if (radio27){
+        radio27 = parseInt(radio27)
+    }
+    if (radio28){
+        radio28 = parseInt(radio28)
+    }
+    if (radio29){
+        radio29 = parseInt(radio29)
+    }
+    if (radio30){
+        radio30 = parseInt(radio30)
+    }
+    if (radio31){
+        radio31 = parseInt(radio31)
+    }
+    if (radio32){
+        radio32 = parseInt(radio32)
+    }
+    if (radio33){
+        radio33 = parseInt(radio33)
+    }
+    if (radio34){
+        radio34 = parseInt(radio34)
+    }
+    if (radio35){
+        radio35 = parseInt(radio35)
+    }
+    if (radio36){
+        radio36 = parseInt(radio36)
+    }
+    if (radio37){
+        radio37 = parseInt(radio37)
+    }
+    if (radio38){
+        radio38 = parseInt(radio38)
+    }
+    if (radio39){
+        radio39 = parseInt(radio39)
+    }
+    if (radio40){
+        radio40 = parseInt(radio40)
+    }
+    if (radio41){
+        radio41 = parseInt(radio41)
+    }
+    if (radio42){
+        radio42 = parseInt(radio42)
+    }
+    if (radio43){
+        radio43 = parseInt(radio43)
+    }
+    if (radio44){
+        radio44 = parseInt(radio44)
+    }
+    if (radio45){
+        radio45 = parseInt(radio45)
+    }
+    if (radio46){
+        radio46 = parseInt(radio46)
+    }
+    if (radio47){
+        radio47 = parseInt(radio47)
+    }
+    if (radio48){
+        radio48 = parseInt(radio48)
+    }
+    if (radio49){
+        radio49 = parseInt(radio49)
+    }
+    if (radio50){
+        radio50 = parseInt(radio50)
+    }
+    if (radio51){
+        radio51 = parseInt(radio51)
+    }
+    if (radio52){
+        radio52 = parseInt(radio52)
+    }
+    if (radio53){
+        radio53 = parseInt(radio53)
+    }
+    if (radio54){
+        radio54 = parseInt(radio54)
+    }
+    if (radio55){
+        radio55 = parseInt(radio55)
+    }
+    if (radio56){
+        radio56 = parseInt(radio56)
+    }
+    if (radio57){
+        radio57 = parseInt(radio57)
+    }
+    if (radio58){
+        radio58 = parseInt(radio58)
+    }
+    if (radio59){
+        radio59 = parseInt(radio59)
+    }
+    if (radio60){
+        radio60 = parseInt(radio60)
+    }
+    if (radio61){
+        radio61 = parseInt(radio61)
+    }
+    if (radio62){
+        radio62 = parseInt(radio62)
+    }
+    if (radio63){
+        radio63 = parseInt(radio63)
+    }
+    if (radio64){
+        radio64 = parseInt(radio64)
+    }
+    if (radio65){
+        radio65 = parseInt(radio65)
+    }
+    if (radio66){
+        radio66 = parseInt(radio66)
+    }
+    if (radio67){
+        radio67 = parseInt(radio67)
+    }
+    if (radio68){
+        radio68 = parseInt(radio68)
+    }
+    if (radio69){
+        radio69 = parseInt(radio69)
+    }
+    if (radio70){
+        radio70 = parseInt(radio70)
+    }
+    if (radio71){
+        radio71 = parseInt(radio71)
+    }
+    if (radio72){
+        radio72 = parseInt(radio72)
+    }
+    if (radio73){
+        radio73 = parseInt(radio73)
+    }
+    if (radio74){
+        radio74 = parseInt(radio74)
+    }
+    if (radio75){
+        radio75 = parseInt(radio75)
+    }
+    if (radio76){
+        radio76 = parseInt(radio76)
+    }
+    if (radio77){
+        radio77 = parseInt(radio77)
+    }
+    if (radio78){
+        radio78 = parseInt(radio78)
+    }
+    if (radio79){
+        radio79 = parseInt(radio79)
+    }
+    if (radio80){
+        radio80 = parseInt(radio80)
+    }
+    if (radio81){
+        radio81 = parseInt(radio81)
+    }
+    if (radio82){
+        radio82 = parseInt(radio82)
+    }
+    if (radio83){
+        radio83 = parseInt(radio83)
+    }
+    if (radio84){
+        radio84 = parseInt(radio84)
+    }
+    if (radio85){
+        radio85 = parseInt(radio85)
+    }
+    if (radio86){
+        radio86 = parseInt(radio86)
+    }
+    if (radio87){
+        radio87 = parseInt(radio87)
+    }
+    if (radio88){
+        radio88 = parseInt(radio88)
+    }
+    if (radio89){
+        radio89 = parseInt(radio89)
+    }
+    if (radio90){
+        radio90 = parseInt(radio90)
+    }
+    
+    total = radio1 + radio2 + radio3 + radio4+ radio5+ radio6+ radio7+ radio8+ radio9+ radio10+ radio11+ radio12+ radio13 +
+    radio14+ radio15 + radio16 + radio17 + radio18 + radio18 + radio20 + radio21 + radio22 + radio23 + radio24 + radio25 +
+    radio26 + radio27 + radio28 + radio29 + radio30 + radio31 + radio32 + radio33 + radio34 + radio35 + radio36 + radio37 + radio38 +
+    radio39 + radio40 + radio41 + radio42 + radio43 + radio44 + radio45 + radio46 + radio47 + radio48 + radio49 + radio50 +
+    radio51 + radio52 + radio53 + radio54 + radio55 + radio56 + radio57 + radio58 + radio59 + radio60 + radio61 + radio62 + 
+    radio63 + radio64 + radio65 + radio66 + radio67 + radio68 + radio69 + radio70 + radio71 + radio72 + radio73 + radio74 +
+    radio75 + radio76 + radio77 + radio78 + radio79 + radio80 + radio81 + radio82 + radio83 + radio84 + radio85 + radio86 +
+    radio87 + radio88 + radio89 + radio90;
+
+    
+
+    resultado.innerHTML = `${total - 1} de 90 respondidas`
+}
